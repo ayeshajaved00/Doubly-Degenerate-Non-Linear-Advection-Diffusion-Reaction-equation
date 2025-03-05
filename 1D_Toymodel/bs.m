54fa75c96c52f022973ac9b8b89e92602482482d
Ayesha Javed <167529945+ayeshajaved00@users.noreply.github.com>
2025-03-05 15:19:30 +0100

    Add files via upload

---
+++ 1D_Toymodel/bs.m
@@ -0,0 +1,20 @@
+function b = bs(S)
+
+n=length(S);
+
+b=zeros(1,n);
+
+% Loop through each element in S to calculate b
+for i = 1:n
+       if S(i) < 0
+           b(i) = 0; 
+       elseif S(i) <= 1/sqrt(2)
+            b(i) = S(i);
+        elseif (S(i) >= 1/sqrt(2)) && (S(i) <= sqrt(2))
+            b(i) = sqrt(1 - (sqrt(2) - S(i))^2);
+        else
+            b(i) = 1;
+        end
+end
+end
+
