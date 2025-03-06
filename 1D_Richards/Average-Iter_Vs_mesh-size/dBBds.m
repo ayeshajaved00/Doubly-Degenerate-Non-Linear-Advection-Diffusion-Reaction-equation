e17fce4a43d70585b00c83dc9bbced7fb34d1969
Ayesha Javed <167529945+ayeshajaved00@users.noreply.github.com>
2025-03-06 11:57:00 +0100

    Delete 1D_Richards/Average-Iter_Vs_mesh-size/dBBds.m

--- 1D_Richards/Average-Iter_Vs_mesh-size/dBBds.m
+++
@@ -1,6 +0,0 @@
-function Bs_derivative = dBBds(s,index_s,B_values)
-    % Compute the derivative of the Bs values with respect to s
-    global du
-    ispos = .5*(sign(s) + 1);  
-    Bs_derivative = 1- ispos + ispos.*(B_values(index_s+1) - B_values(index_s)) / du;
-end
