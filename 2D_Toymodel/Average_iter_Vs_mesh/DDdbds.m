7960f5003b1e89eaff79d564ff0bc624f1748f04
Ayesha Javed <167529945+ayeshajaved00@users.noreply.github.com>
2025-03-13 12:22:09 +0100

    DDdbds.m

--- 2D_Toymodel/Average_iter_Vs_mesh/DDdbds.m
+++ 2D_Toymodel/Average_iter_Vs_mesh/DDdbds.m
@@ -7,11 +7,9 @@ bpp = zeros(n, dim);
 % Loop through each element in S to calculate bpp
 for i = 1:n
     for j = 1:dim
-        if S(i, j) <= 0
-            bpp(i, j) = 0; 
-        elseif S(i, j) < 1/sqrt(2)
+        if S(i, j) < 1/sqrt(2)
             bpp(i, j) = 1;
-        elseif (S(i, j) > 1/sqrt(2)) && (S(i, j) < sqrt(2))
+        elseif (S(i, j) >= 1/sqrt(2)) && (S(i, j) < sqrt(2))
             bpp(i, j) = (sqrt(2) - S(i, j)) / sqrt(1 - (sqrt(2) - S(i, j))^2);
         else
             bpp(i, j) = 0;
