4109fef0f2b326f8b8087c463489383c6c2fd12b
Ayesha Javed <167529945+ayeshajaved00@users.noreply.github.com>
2025-03-13 11:22:35 +0100

    BBs.m

--- 1D_Toymodel/Contraction_Rate/BBs.m
+++ 1D_Toymodel/Contraction_Rate/BBs.m
@@ -1,16 +1,17 @@
 function B = BBs(S)
-n=length(S);
-B=zeros(1,n);
+    n = length(S);
+    B = zeros(1, n);
 
-for i = 1:n
+    for i = 1:n
         if S(i) <= 0
-            B(i) = S(i);
+            B(i) = 0; 
         elseif (S(i) >= 0) && (S(i) <= 1/sqrt(2))
-            B(i) =  1-sqrt(1-S(i).^2);
-         else
-             B(i) = S(i)+1-sqrt(2);
+            B(i) = 1 - sqrt(1 - S(i).^2);
+        else
+            B(i) = S(i) + 1 - sqrt(2);
         end
+    end
 end
-end
+
 
    
