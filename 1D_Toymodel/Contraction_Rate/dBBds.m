4583c15554245446f406d880772a60d833cfdbd4
Ayesha Javed <167529945+ayeshajaved00@users.noreply.github.com>
2025-03-13 11:23:32 +0100

    dBBds.m

--- 1D_Toymodel/Contraction_Rate/dBBds.m
+++ 1D_Toymodel/Contraction_Rate/dBBds.m
@@ -1,14 +1,15 @@
 function Bp = dBBds(S)
-n=length(S);
-Bp=zeros(1,n);
+    n = length(S);
+    Bp = zeros(1, n);
 
-for i = 1:n
-        if S(i) < 0
-            Bp(i) = 1;
-        elseif (S(i) > 0) && (S(i) < 1/sqrt(2))
-            Bp(i) = S(i)./(sqrt(1-S(i).^2));
+    for i = 1:n
+        if S(i) <= 0
+            Bp(i) = 0;  % Derivative is zero for S <= 0
+        elseif (S(i) > 0) && (S(i) <= 1/sqrt(2))
+            Bp(i) = S(i) ./ sqrt(1 - S(i).^2);  % Derivative in region 0 < S <= 1/sqrt(2)
         else
-            Bp(i) = 1;
+            Bp(i) = 1;  % Derivative is 1 for S > 1/sqrt(2)
         end
+    end
 end
-end
+
