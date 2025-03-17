b70c82742b63e8072de9e55c27eaad271e58ef5b
Ayesha Javed <167529945+ayeshajaved00@users.noreply.github.com>
2025-03-17 13:41:16 +0100

    PdBBds.m

--- 2D_PME/Average_iter_Vs_mesh/PdBBds.m
+++ 2D_PME/Average_iter_Vs_mesh/PdBBds.m
@@ -1,9 +1,4 @@
 function BBp = PdBBds(S, m)
-% PdBBds: Calculate the derivative of B(S) with respect to S.
-% S: a 2D array of size nx+1 x ny+1
-% m: a scalar value
-% Returns:
-% BBp: a 2D array of size nx+1 x ny+1 containing the computed derivative values.
 
 [n, dim] = size(S); % Get the size of the input matrix
 
@@ -12,10 +7,8 @@ T = (1/m)^(1/(m-1)); % Threshold value based on m
 
 for i = 1:n
     for j = 1:dim
-        if S(i,j) < 0
-            BBp(i,j) = 1; % If S < 0, B'(S) = 1
-        elseif S(i,j) < T
-            BBp(i,j) = m * S(i,j).^(m-1); 
+        if S(i,j) <= T
+            BBp(i,j) = m .* S(i,j).^(m-1); 
         else
             BBp(i,j) = 1; 
         end
