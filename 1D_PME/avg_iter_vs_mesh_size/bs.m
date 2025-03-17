e3d00045f050c95495899f4e72e4c034be8a10bc
Ayesha Javed <167529945+ayeshajaved00@users.noreply.github.com>
2025-03-17 13:30:51 +0100

    bs.m

--- 1D_PME/avg_iter_vs_mesh_size/bs.m
+++ 1D_PME/avg_iter_vs_mesh_size/bs.m
@@ -2,12 +2,10 @@ function b = bs(S,m)
 
 n=length(S);
 b=zeros(1,n);
-T = (1/m)^(1/(m-1)); 
+T = (1/m)^(1/(m-1));
 
 for i = 1:n
-        if S(i) < 0
-            b(i) = 0; 
-        elseif S(i) <= T
+        if S(i) <= T
             b(i) = S(i); 
         else
             b(i) = (S(i) - T + T^m).^(1/m); 
