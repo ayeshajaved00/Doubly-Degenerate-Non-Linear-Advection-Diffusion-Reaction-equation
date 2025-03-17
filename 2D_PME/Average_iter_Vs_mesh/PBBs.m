24ee3d6f119e97acc27f7ef76364f1c8e68a2d9c
Ayesha Javed <167529945+ayeshajaved00@users.noreply.github.com>
2025-03-17 13:39:59 +0100

    PBBs.m

--- 2D_PME/Average_iter_Vs_mesh/PBBs.m
+++ 2D_PME/Average_iter_Vs_mesh/PBBs.m
@@ -1,9 +1,4 @@
 function B = PBBs(S, m)
-% PBBs: Calculate B values based on S and parameter m with specific conditions.
-% S: a 2D array of size nx+1 x ny+1
-% m: a scalar value
-% Returns:
-% B: a 2D array of size nx+1 x ny+1 containing the computed B values.
 
 [n, dim] = size(S); % Get the size of the input matrix
 
@@ -12,9 +7,7 @@ T = (1/m)^(1/(m-1));
 
 for i = 1:n
     for j = 1:dim
-        if S(i,j) < 0
-            B(i,j) = S(i,j); % If S < 0, B = S
-        elseif S(i,j) <= T
+        if S(i,j) <= T
             B(i,j) = (S(i,j)).^m; 
         else
             B(i,j) = S(i,j) - T + T^m; 
