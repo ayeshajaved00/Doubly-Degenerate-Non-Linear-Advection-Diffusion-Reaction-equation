69c30174cdee02fafddb4ffeabbc8b1c89215119
Ayesha Javed <167529945+ayeshajaved00@users.noreply.github.com>
2025-03-17 13:33:46 +0100

    dbds.m

--- 1D_PME/contraction_rate/dbds.m
+++ 1D_PME/contraction_rate/dbds.m
@@ -6,12 +6,10 @@ b=zeros(1,n);
 T = (1/m)^(1/(m-1)); 
 
 for i = 1:n
-        if S(i) < 0
-            b(i) = 0; % If S < 0, b'(S) = 0
-        elseif S(i) < T
+        if S(i) <= T
             b(i) = 1; 
         else
-            b(i) = (1/m) * (S(i) - T + T^m)^(1/m - 1); 
+            b(i) = (1/m) .* (S(i) - T + T^m).^(1/m - 1); 
         end
 end
 end
