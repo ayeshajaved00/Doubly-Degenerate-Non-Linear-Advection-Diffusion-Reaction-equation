d0620057c0dde0fedb5d1b9d663abdbfeba7c59b
Ayesha Javed <167529945+ayeshajaved00@users.noreply.github.com>
2025-03-17 13:32:44 +0100

    BBs.m

--- 1D_PME/contraction_rate/BBs.m
+++ 1D_PME/contraction_rate/BBs.m
@@ -4,9 +4,7 @@ B=zeros(1,n);
 T = (1/m)^(1/(m-1));
 
 for i = 1:n
-        if S(i) < 0
-            B(i) = S(i); 
-        elseif S(i) <= T
+        if S(i) <= T
             B(i) = (S(i)).^m; 
         else
             B(i) = S(i) - T + T^m; 
