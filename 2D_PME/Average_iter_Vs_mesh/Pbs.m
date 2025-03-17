6c421b3eb2c95af3201a93e771ce5530f3332761
Ayesha Javed <167529945+ayeshajaved00@users.noreply.github.com>
2025-03-17 13:40:32 +0100

    Pbs.m

--- 2D_PME/Average_iter_Vs_mesh/Pbs.m
+++ 2D_PME/Average_iter_Vs_mesh/Pbs.m
@@ -1,9 +1,4 @@
 function b = Pbs(S, m)
-% Pbs: Compute the function B(S) based on the input S and parameter m.
-% S: a 2D array of size nx+1 x ny+1
-% m: a scalar value
-% Returns:
-% b: a 2D array of the same size as S with the computed values.
 
 [n, dim] = size(S); % Get the size of the input matrix
 
@@ -12,9 +7,7 @@ T = (1/m)^(1/(m-1)); % Threshold value based on m
 
 for i = 1:n
     for j = 1:dim
-        if S(i,j) < 0
-            b(i,j) = 0; % If S < 0, B(S) = 0
-        elseif S(i,j) <= T
+        if S(i,j) <= T
             b(i,j) = S(i,j); 
         else
             b(i,j) = (S(i,j) - T + T^m).^(1/m); 
